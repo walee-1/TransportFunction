f80ccf0163a30bd103909fef79b9ab76bc5ba235
Synchrotronus <moser.dani@hotmail.de>
2019-11-14 14:25:33 +0100

    Neutron beam introduction with different methods + Pitch inclusion

---
+++ NeutronBeam/NeutronBeamDefinition.m
@@ -0,0 +1,39 @@
+
+(*to show how I come to the description*)
+FullSimplify[
+ Solve[{0 == k1*x1 + d1 && y2 == k1*x2 + d1 && pl == x3 - x2 && 
+    tw == pl + x4 - x3 + x2 - x1 && y2 == k2*x2 + d2 && 
+    y3 == k2*x3 + d2 && y3 == k3*x3 + d3 && 0 == k3*x4 + d3 && 
+    x1 == -tw/2 && x4 == tw/2 && k1 > 0 && 
+    k2 < 0 && {k1, x1, d1, y2, k1, x2, pl, x3, tw, x4, d2, y3, 
+      k3} \[Element] Reals && pl < tw}, {x1, x2, x3, x4, y2, y3, d1, 
+   d2, d3}]];
+   
+   
+   TrapezNBeamNew[xn_, {tw_, pl_, k1_, k2_, k3_}] := Piecewise[
+  {
+   {0, xn < -tw/2},
+   {k1*xn + k1*tw/2, -tw/2 <= 
+     xn < -((2 k2 pl - 2 k3 pl + (k1 + k3) tw)/(2 (k1 - k3)))},
+   {k2*xn + (-2 (k1 - k2) (k2 - k3) pl + (k1 (k2 - 2 k3) + 
+         k2 k3) tw)/(
+     2 (k1 - k3)), -((2 k2 pl - 2 k3 pl + (k1 + k3) tw)/(
+      2 (k1 - k3))) <= 
+     xn < -((2 (-k1 + k2) pl + (k1 + k3) tw)/(2 (k1 - k3)))},
+   {k3*xn - (k3 tw)/
+     2, -((2 (-k1 + k2) pl + (k1 + k3) tw)/(2 (k1 - k3))) <= xn <= 
+     tw/2},
+   {0, xn > tw/2}
+   }
+  ]
+  
+  TrapezNBeamNewNorm[{tw_, pl_, k1_, k2_, k3_}] := 
+ TrapezNBeamNewNorm[{tw, pl, k1, k2, k3}] = 
+  NIntegrate[
+   TrapezNBeamNew[xn, {tw, pl, k1, k2, k3}], {xn, -tw/2, tw/2}]
+   
+   
+   TrapezNBeamNewNormed[xn_, {tw_, pl_, k1_, k2_, k3_}] := 
+ TrapezNBeamNew[xn, {tw, pl, k1, k2, k3}]/
+  TrapezNBeamNewNorm[{tw, pl, k1, k2, k3}]
+   
