18afd8a13929651c74c73f62d9d7adda1f502d24
Waleed Khalid <wal.ee@live.com>
2021-03-24 11:39:35 +0100

    more results and resultcheck notebook

--- MergerTransport/SC_Prot_opt_da0/a-1040/Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_script.ps1
+++ MergerTransport/SC_Prot_opt_da0/a-1040/Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_script.ps1
@@ -26,3 +26,10 @@ F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-
 F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P4.m >> a-1040.out
 F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P5.m >> a-1040.out
 F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P6.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P0.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P1.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P2.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P3.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P4.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P5.m >> a-1040.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1040_P6.m >> a-1040.out
--- MergerTransport/SC_Prot_opt_da0/a-1055/Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_script.ps1
+++ MergerTransport/SC_Prot_opt_da0/a-1055/Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_script.ps1
@@ -26,3 +26,10 @@ F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-
 F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P4.m >> a-1055.out
 F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P5.m >> a-1055.out
 F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P6.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P0.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P1.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P2.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P3.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P4.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P5.m >> a-1055.out
+F:\"Program Files"\"Wolfram Research"\Mathematica\11.3\math -script Transfer_08-09-20_SC_Prot_opt_da0_G_a-1055_P6.m >> a-1055.out
